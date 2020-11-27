3584d21157c1d06f0cf04e8070e03e00ee661275
Damien Vesper <ldamienvesper@gmail.com>
2020-11-26 19:27:53 -0600

    RARE WINDOWS BATCH FILE

--- events/ready.js
+++ events/ready.js
@@ -6,7 +6,7 @@ const log = require(`../utils/log.js`);
 
 // Event execution.
 module.exports = client.on(`ready`, () => {
-    log(`${client.user.tag} has started with ${client.users.size} in ${client.guilds.size} servers.`, `green`);
+    log(`${client.user.tag} has started with ${client.users.size} users in ${client.guilds.size} servers.`, `green`);
     client.user.setPrescence({
         game: {
             name: `${client.users.size} players mine in ${client.user.username}`,
