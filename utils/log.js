b2292f95bf8b870a3a9530ea25e8548ac8aec7d5
Damien Vesper <ldamienvesper@gmail.com>
2021-01-07 17:31:29 -0600

    remove extra stuff

--- utils/log.js
+++ utils/log.js
@@ -69,46 +69,6 @@ module.exports = (color, ...content) => {
     if(!logColor) {
         throw `Did not specify a valid color`;
     }
-
-    // If discordMessage is defined
-    if(discordMessage) {
-        // If the message contains an embed, log it as an embed with it`s title, author, or description if applicable
-        if(discordMessage.embeds.length > 0) {
-            if(discordMessage.embeds[0].title) {
-                logContent = `{Embed: Title = ${discordMessage.embeds[0].title}}`.replace(/[^ -~]+/g, ``);
-
-            } else if(discordMessage.embeds[0].author && discordMessage.embeds[0].author.name) {
-                logContent = `{Embed: Author = ${discordMessage.embeds[0].author.name}}`.replace(/[^ -~]+/g, ``);
-
-            } else if(discordMessage.embeds[0].description) {
-                logContent = `{Embed: Description = ${discordMessage.embeds[0].description}}`.replace(/[^ -~]+/g, ``);
-
-            } else {
-                logContent = `{Embed}`
-            }
-        }
-
-        // If the user option is defined, append their tag to the start of logContent
-        if(options.user) logContent = `[${discordMessage.author.tag}] ` + logContent;
-
-        // If the channel option is defined, append the channel name to the start of logContent
-        if(options.channel && discordMessage.channel.type !== `dm`) logContent = `Channel: ${discordMessage.channel.name} | ` + logContent;
-
-        // If the server option is defined, append the server name to the start of logContent
-        if(options.server) {
-            // If the message is in a guild
-            if(discordMessage.channel.type !== `dm`) {
-                logContent = `Server: ${discordMessage.guild.name} | ` + logContent;
-            // If the message is in a DM
-            } else {
-                logContent = `Server: DM | ` + logContent;
-            }
-        }
-
-        // If the regex option is defined, apply the regex to logContent.
-        if(options.regex) logContent = logContent.replace(/[^ -~]+/g, ``);
-    }
-    
     
     // Log logContent with the color specified to console
     logFile.write(`[${formattedTime}] >> ${logContent}`.replace(/\r?\n|\r/g, ``) + `\n`);
