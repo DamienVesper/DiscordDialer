970fc1241690370fe0f4495a08a8c0218a5bb769
Damien Vesper <ldamienvesper@gmail.com>
2021-02-18 20:05:52 -0600

    lint dial, dtmf

--- commands/dial.js
+++ commands/dial.js
@@ -1,9 +1,6 @@
 // Require modules.
 const config = require(`../config/config`);
-const Discord = require(`discord.js`);
-const {
-    exec
-} = require(`child_process`);
+const { exec } = require(`child_process`);
 const log = require(`../utils/log.js`);
 
 // Command data.
@@ -23,7 +20,7 @@ module.exports.execute = async (client, message, args) => {
 
     if (client.callStatus) return message.channel.send(`${m} ${config.emojis.no} A call is currently taking place!`);
 
-    if (args[0] == `balance`) {
+    if (args[0] === `balance`) {
         message.channel.send(`${m} ${config.emojis.telephone} Dialing balance check...`);
 
         exec(`dial.bat *225`, (err, data) => {
@@ -35,15 +32,15 @@ module.exports.execute = async (client, message, args) => {
         setTimeout(() => {
             message.channel.send(`${config.emojis.ok} Balance check has ended...`);
         }, 11e3);
-    } else if (args[0] == `echotest`) {
+    } else if (args[0] === `echotest`) {
         message.channel.send(`${config.emojis.telephone} Dialing echotest...`);
         exec(`dial.bat 4443`, (err, data) => {
-            if (err) log(`red`, error);
+            if (err) log(`red`, err.stack);
             log(`magenta`, `${message.author.tag} dialed number: ECHOTEST (4443).`);
         });
         client.callStatus = true;
     } else {
-        if (args[0].length != 10 || isNaN(parseInt(args[0]))) return message.channel.send(`${m} ${config.emojis.warning} Invalid Number Format - Please enter a 10 digit US phone number without the "+1".`);
+        if (args[0].length !== 10 || isNaN(parseInt(args[0]))) return message.channel.send(`${m} ${config.emojis.warning} Invalid Number Format - Please enter a 10 digit US phone number without the "+1".`);
         if (config.blacklistedNumbers.includes(`1${args[0]}`)) return message.channel.send(`${m} ${config.emojis.no} That number is blacklisted!`);
 
         exec(`dial.bat ${args[0]}`, (err, data) => {
